350166c4896d2f2d9515ca12e665621d78bdfca2
Marlon Ramírez - Zhionit <zhionit@live.com>
2016-08-23 19:33:54 -0500

    Se crea la clase Cliente, con los datos basicos

---
+++ GymMaster/src/java/gym/Client.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gym;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class Client {
+    
+    
+    private String id;
+    
+    private String name;
+    
+    private Date birthDate;
+    
+    private String address;
+    
+    private List<String> phones;
+    
+    private String healthCare;
+    
+}
