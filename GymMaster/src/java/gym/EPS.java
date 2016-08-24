facd840adc7fa6d9f2e2b3dbb00ab4eed8ec4dbb
ArmandoNCM <Michael@Dell-XPS8500>
2016-08-23 20:10:56 -0500

    Se añadieron detalles y descripción a las clases EPS y TipoSangre

--- GymMaster/src/java/gym/EPS.java
+++ GymMaster/src/java/gym/EPS.java
@@ -1,12 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package gym;
 
 /**
- *
+ * Esta clase representa una Empresa Prestadora de Salud a la cual
+ * estaría afiliado un cliente
  * @author Michael
  */
 public class EPS {
