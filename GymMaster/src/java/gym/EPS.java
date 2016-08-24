aaf5cc62b4911d7085c40ab933278cc4f05532cd
ArmandoNCM <Michael@Dell-XPS8500>
2016-08-23 19:47:16 -0500

    Se creó la clase EPS

---
+++ GymMaster/src/java/gym/EPS.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gym;
+
+/**
+ *
+ * @author Michael
+ */
+public class EPS {
+    
+    /**
+     * Identificador de la EPS
+     */
+    private String id;
+    
+    /**
+     * Nombre de la EPS
+     */
+    private String nombre;
+    
+}
