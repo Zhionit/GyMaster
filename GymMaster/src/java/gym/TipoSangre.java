970eb46a1df378a5d4520db3f8bf55cee981efa3
ArmandoNCM <Michael@Dell-XPS8500>
2016-08-23 22:06:34 -0500

    Se desacoplaron las constantes de Tipos de sangre a una interfaz

--- GymMaster/src/java/gym/TipoSangre.java
+++ GymMaster/src/java/gym/TipoSangre.java
@@ -7,23 +7,6 @@ package gym;
 public class TipoSangre {
     
     /**
-     * Constante que representa el tipo de sangre A
-     */
-    public static final byte TIPO_A = 0;
-    /**
-     * Constante que representa el tipo de sangre B
-     */
-    public static final byte TIPO_B = 1;
-    /**
-     * Constante que representa el tipo de sangre AB
-     */
-    public static final byte TIPO_AB = 2;
-    /**
-     * Constante que representa el tipo de sangre O
-     */
-    public static final byte TIPO_O = 3;
-    
-    /**
      * Tipo de sangre
      */
     private byte tipo;
---
+++ GymMaster/src/java/gym/TiposSangre.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gym;
+
+/**
+ *
+ * @author Michael
+ */
+public interface TiposSangre {
+    /**
+     * Constante que representa el tipo de sangre A
+     */
+    public static final byte TIPO_A = 0;
+    /**
+     * Constante que representa el tipo de sangre B
+     */
+    public static final byte TIPO_B = 1;
+    /**
+     * Constante que representa el tipo de sangre AB
+     */
+    public static final byte TIPO_AB = 2;
+    /**
+     * Constante que representa el tipo de sangre O
+     */
+    public static final byte TIPO_O = 3;
+    
+}
