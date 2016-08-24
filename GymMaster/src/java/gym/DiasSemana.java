52172e2496e76971a00bc19f0107adcc115afda5
ArmandoNCM <Michael@Dell-XPS8500>
2016-08-23 22:24:32 -0500

    Se creo una interfaz para numerar los días de la semana

---
+++ GymMaster/src/java/gym/DiasSemana.java
@@ -0,0 +1,16 @@
+package gym;
+
+/**
+ * Días de la semana numerados segun el  ISO 8601
+ * "Sunday is the seventh and last day of the week."
+ * @author Michael
+ */
+public interface DiasSemana {
+    public static final int LUNES = 1;
+    public static final int MARTES = 2;
+    public static final int MIERCOLES = 3;
+    public static final int JUEVES = 4;
+    public static final int VIERNES = 5;
+    public static final int SABADO = 6;
+    public static final int DOMINGO = 7;
+}
