677e3036adba17f5a568e80915c678c721eeef20
Marlon Ramírez - Zhionit <zhionit@live.com>
2016-08-24 23:06:38 -0500

    - Se crean los atributos del Bean: EPSBean - Se documentan los métodos de dicha clase

--- GymMaster/src/java/beans/EPSBean.java
+++ GymMaster/src/java/beans/EPSBean.java
@@ -5,6 +5,9 @@
  */
 package beans;
 
+import gym.EPS;
+import java.util.ArrayList;
+import java.util.List;
 import javax.inject.Named;
 import javax.enterprise.context.Dependent;
 
@@ -16,13 +19,78 @@ import javax.enterprise.context.Dependent;
 @Dependent
 public class EPSBean 
 {
-
+    // Atributos
+    
     /**
-     * Creates a new instance of EPSBean
+     * Instancia que representa un objeto de la clase EPS
+     */
+    private EPS eps;
+    
+    /**
+     * Lista de EPS's 
+     */
+    private List <EPS> lstEPSs = new ArrayList<EPS>( );
+    
+    /**
+     * Crea una nueva instancia de EPSBean
      */
     public EPSBean() 
     {
-        
+        eps = new EPS();
     }
+    
+    /**
+     * Crea una nueva instancia de una EPSBEan
+     * @param id identificador de la EPS
+     * @param nombre nombre de la EPS
+     */
+    public EPSBean(int id, String nombre)
+    {
+        eps = new EPS(id, nombre);
+    }
+
+    /**
+     * Obtiene la EPS de EPSBean
+     * 
+     * @return EPS - 
+     */
+    public EPS getEps() 
+    {
+        return eps;
+    }
+
+    /**
+     * Modifica la EPS del EPSBEan
+     * @param eps nueva EPS
+     */
+    public void setEps(EPS eps) 
+    {
+        this.eps = eps;
+    }
+
+    /**
+     * Obtiene la lista de las EPS's del EPSBean
+     * @return List<EPS> La lista de EPS de EPSBean
+     */
+    public List<EPS> getLstEPSs() 
+    {
+        return lstEPSs;
+    }
+
+    /**
+     * Modifica la lista de EPS's que tiene el Bean
+     * @param List<EPS> lstEPSs La nueva lista de EPS's Para el Bean
+     */
+    public void setLstEPSs(List<EPS> lstEPSs) 
+    {
+        this.lstEPSs = lstEPSs;
+    }
+    
+    public void registrarEPS()
+    {
+        lstEPSs.add(this.eps);
+    }
+    
+    
     
 }
