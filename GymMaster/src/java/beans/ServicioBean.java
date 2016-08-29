e6a59b0f05eb0bbf3fbf5781745f05c0a1474d6c
Marlon Ramírez - Zhionit <zhionit@live.com>
2016-08-29 09:31:29 -0500

    - Se crea el Bean de Servicio

---
+++ GymMaster/src/java/beans/ServicioBean.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beans;
+
+import javax.inject.Named;
+import javax.enterprise.context.Dependent;
+
+/**
+ *
+ * @author zhion
+ */
+@Named(value = "servicioBean")
+@Dependent
+public class ServicioBean 
+{
+
+    /**
+     * Creates a new instance of ServicioBean
+     */
+    public ServicioBean() 
+    {
+        
+    }
+    
+    
+    
+}
